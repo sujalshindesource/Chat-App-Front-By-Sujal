7f883fae98da05bd2a850f70238d2e12fa63d4a1
sujalshindesource <siddheshshinde42556@gmail.com>
2025-08-07 00:28:19 +0530

    here we are again again again

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BACKEND_URL: process.env.REACT_APP_BACKEND_URL || "https://web-production-926e5.up.railway.app/"
+  BACKEND_URL: process.env.REACT_APP_BACKEND_URL || "https://web-production-926e5.up.railway.app"
 };
 
 export default config;
